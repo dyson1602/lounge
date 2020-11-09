1881ad814bd3b116e6932e45c17adfc549e24180
Jaquan1314 <javaughngrant@gmail.com>
2020-11-09 14:33:49 -0500

    post controller and views done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,55 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @user = User.find(@post.user_id)
+  end
+
+  def new
+    @post = Post.new
+    @categories = ["Articles", "Videos", "Tips", "Chairs", "Profiles"]
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      flash[:errors] = @post.errors.full_messages
+      redirect_to new_post_path
+    end
+  end
+
+  def edit
+
+  end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      flash[:errors] = @post.errors.full_messages
+      redirect_to edit_post_path
+    end
+  end
+
+  def destroy
+    id = @post.user_id
+    @post.destroy
+    redirect_to user_path(id)
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:user_id, :content_url, :category)
+  end
 end
