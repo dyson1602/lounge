682bb5beb6b0acced66d1a5fd3ff96a1ef7cb689
dyson1602 <cclark000@citymail.cuny.edu>
2020-11-09 13:14:24 -0500

    seed data built. relationships tested. all function properly:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+PostComment.destroy_all
+
+contents = ["https://www.google.com/", "https://twitter.com/home", "https://www.twitch.tv/directory", "https://www.youtube.com/"]
+categories = ["Articles", "Videos", "Tips", "Chairs", "Profiles"]
+
+5.times do
+    User.create(name: Faker::Name.name, mod: rand(1...5), bio: Faker::Movies::Ghostbusters.quote, 
+    location: Faker::Nation.capital_city)
+end
+
+10.times do
+    Post.create(user_id: User.all.sample.id, content_url: contents.sample, category: categories.sample)
+end
+
+15.times do
+    Comment.create(user_id: User.all.sample.id, user_comment: Faker::Games::Overwatch.quote)
+end
+
+15.times do
+    PostComment.create(comment_id: Comment.all.sample.id, post_id: Post.all.sample.id)
+end
